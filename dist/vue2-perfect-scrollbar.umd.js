e26e25a996df16dfdbdc338ceabafe94cef75f31
Emīls M <emils.malovka@magebit.com>
2020-08-20 21:25:42 +0300

    Update vue2-perfect-scrollbar.umd.js

--- dist/vue2-perfect-scrollbar.umd.js
+++ dist/vue2-perfect-scrollbar.umd.js
@@ -117,7 +117,7 @@
       this.handlers[eventName] = [];
     }
     this.handlers[eventName].push(handler);
-    this.element.addEventListener(eventName, handler, false);
+    this.element.addEventListener(eventName, handler, {passive: false});
   };
 
   EventElement.prototype.unbind = function unbind (eventName, target) {
@@ -127,7 +127,7 @@
       if (target && handler !== target) {
         return true;
       }
-      this$1.element.removeEventListener(eventName, handler, false);
+      this$1.element.removeEventListener(eventName, handler, {passive: false});
       return false;
     });
   };
