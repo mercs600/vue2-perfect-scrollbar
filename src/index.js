9b62a76102a20d11a53d85e0a9104f7ff842c2df
mercs600 <kontakt@mercs600.pl>
2020-10-28 18:23:09 +0100

    New plugin schema for vue3

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import PerfectScrollbar from './Scrollbar'
 
-export function install (Vue, settings) {
-  if (settings) {
-    if (settings.name && typeof settings.name === 'string') {
-      PerfectScrollbar.name = settings.name
-    }
+export default {
+  install: (Vue, settings) => {
+    if (settings) {
+      if (settings.name && typeof settings.name === 'string') {
+        PerfectScrollbar.name = settings.name
+      }
 
-    if (settings.options && typeof settings.options === 'object') {
-      PerfectScrollbar.props.options.default = () => {
-        return settings.options
+      if (settings.options && typeof settings.options === 'object') {
+        PerfectScrollbar.props.options.default = () => {
+          return settings.options
+        }
+      }
+
+      if (settings.tag && typeof settings.tag === 'string') {
+        PerfectScrollbar.props.tag.default = settings.tag
+      }
+
+      if (settings.watchOptions && typeof settings.watchOptions === 'boolean') {
+        PerfectScrollbar.props.watchOptions = settings.watchOptions
       }
     }
 
-    if (settings.tag && typeof settings.tag === 'string') {
-      PerfectScrollbar.props.tag.default = settings.tag
-    }
-
-    if (settings.watchOptions && typeof settings.watchOptions === 'boolean') {
-      PerfectScrollbar.props.watchOptions = settings.watchOptions
-    }
+    Vue.component(
+      PerfectScrollbar.name,
+      PerfectScrollbar
+    )
   }
-
-  Vue.component(
-    PerfectScrollbar.name,
-    PerfectScrollbar
-  )
 }
 
 export { PerfectScrollbar }
-export default install
